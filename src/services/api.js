e9de94d2e88cbbd256e952eb860c2a33d02cc0aa
deepak90221 <116907628+deepak90221@users.noreply.github.com>
2026-08-29 22:54:05 +0530

    updated api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-baseURL: "http://localhost:8888"
+    baseURL: "https://springbootjobportal-production-3d87.up.railway.app"
 });
 
 export default API;
